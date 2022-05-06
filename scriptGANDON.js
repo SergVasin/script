13b9d6cd15b88497097f6354f665d5ab26f10012
SergVasin <66994038+SergVasin@users.noreply.github.com>
2022-05-06 14:26:29 +0400

    Create scriptGANDON.js

---
+++ scriptGANDON.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    setTimeout(function() {
+//     $("button").remove();
+//     $("button").css({'pointer-events' : 'none'});
+//     $("a").css({'pointer-events' : 'none'});
+//     $("a[href]").attr('href', 'https://yandex.ru/');
+//        $("form button").removeAttr('type');
+     }, 3000); 
+});
