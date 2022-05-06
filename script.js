16c0de0ea07d1eefa9877be8a0acafa02ad48155
SergVasin <66994038+SergVasin@users.noreply.github.com>
2022-05-06 13:38:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 //     $("button").remove();
 //     $("button").css({'pointer-events' : 'none'});
 //     $("a").css({'pointer-events' : 'none'});
-       $("a[href]").attr('href', 'https://yandex.ru/')
+//     $("a[href]").attr('href', 'https://yandex.ru/');
+       $("form button").removeAttr('type');
      }, 3000); 
 });
