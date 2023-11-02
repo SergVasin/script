8849bbd8809763b6f8deefa24a373a52a8ed1542
SergVasin <66994038+SergVasin@users.noreply.github.com>
2023-11-02 11:46:37 +0400

    Update redis.js

--- redis.js
+++ redis.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     setTimeout(function() {
 //     $("button").remove();
 //     $("button").css({'pointer-events' : 'none'});
-    $("a").css({'pointer-events' : 'none'});
+    // $("a").css({'pointer-events' : 'none'});
 //     $("a[href]").attr('href', 'https://yandex.ru/');
 //        $("form button").removeAttr('type');
      }, 3000); 
