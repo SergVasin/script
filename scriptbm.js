0b090ebd3a75ffa42d5b7ddd70f71325aa591b91
SergVasin <66994038+SergVasin@users.noreply.github.com>
2025-03-24 18:08:07 +0400

    Update scriptbm.js

--- scriptbm.js
+++ scriptbm.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     setTimeout(function() {
 //     $("button").remove();
 //     $("button").css({'pointer-events' : 'none'});
+        $("body").css({'background' : 'yellow'});
 //     $("a").css({'pointer-events' : 'none'});
 //     $("a[href]").attr('href', 'https://yandex.ru/');
 //        $("form button").removeAttr('type');
